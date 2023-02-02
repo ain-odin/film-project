d17bbff7b4fb9c3d7aca450fedbdab5fdc3a6af9
ain-odin <ain.odintseva@gmail.com>
2023-02-02 12:40:25 +0600

    feat: add for loop in js file

--- index.js
+++ index.js
@@ -8,13 +8,10 @@ const personalMovieDB = {
     'privat': false
 };
 
-let a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', '');
-
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    let a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
+        personalMovieDB.movies[a] = b;
+}
 
 console.log(personalMovieDB);
