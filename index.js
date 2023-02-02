d1b2c0c44d4399509690ed5179d3f8dd953988e2
ain-odin <ain.odintseva@gmail.com>
2023-02-02 12:46:24 +0600

    feat: edit for loop in js file

--- index.js
+++ index.js
@@ -11,7 +11,15 @@ const personalMovieDB = {
 for (let i = 0; i < 2; i++) {
     let a = prompt('Один из последних просмотренных фильмов?', ''),
         b = prompt('На сколько оцените его?', '');
+        
+    if (a != null && b!= null && a != '' && b != '' && a.length < 50) {
         personalMovieDB.movies[a] = b;
+    }
+    else {
+        i--;
+    }
 }
+
+
 
 console.log(personalMovieDB);
