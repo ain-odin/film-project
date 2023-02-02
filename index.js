aef9511c15928d1a544e2af7eb0f5789f5fbc8ee
ain-odin <ain.odintseva@gmail.com>
2023-02-02 11:39:16 +0600

    feat: edit js file

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     'count': numberOfFilms,
@@ -8,9 +8,13 @@ const personalMovieDB = {
     'privat': false
 };
 
-let movieName = prompt('Один из последних просмотренных фильмов?', '');
-let movieScore = prompt('На сколько оцените его?', '');
+let a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies = {
-    movieName: movieScore
-};
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
